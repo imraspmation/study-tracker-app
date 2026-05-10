dd35d5218740f9cb3a027251d2a9eb0bb9de5962
pb <gottin.job@gmail.com>
2026-05-10 16:09:26 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+import RecordForm from './RecordForm'
+
+function App() {
+
+  return (
+      <>
+	  <RecordForm />
+      </>
+  )
+}
+
+export default App
---
+++ src/RecordList.jsx
@@ -0,0 +1,7 @@
+export default function RecordList() {
+    return (
+	<>
+	    <h1>学習記録一覧</h1>
+	</>
+    )
+}
